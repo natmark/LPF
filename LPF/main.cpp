7dd10986065f52687cee92c4a43201b718a44b1e
natmark <natmark0918@gmail.com>
2017-06-08 17:46:58 +0900

    :art: Use FFT

--- LPF/main.cpp
+++ LPF/main.cpp
@@ -24,48 +24,48 @@ const float Kernel[3][3] = {
 
 int main(int argc, const char * argv[])
 {
-    IplImage* loadImg = cvLoadImage("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    if(loadImg == NULL) return 0;
+    // https://stackoverflow.com/questions/10269456/inverse-fourier-transformation-in-opencv
+    // http://org-technology.com/posts/smoother.html
     
-    cv::Mat mat(loadImg);  //copy constructor
-
-    cv::Mat src = loadImg; //source
-    cv::Mat dst = src.clone(); // destination
+    // Load an image
+    cv::Mat inputImage = cv::imread("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", 0);
     
-    //initialize pixel value
-    for(int y = 0; y < src.rows; y++)
-        for(int x = 0; x < src.cols; x++)
-            dst.at<uchar>(y,x) = 0.0;
+    // Convert to float
+    cv::Mat fImage;
+    inputImage.convertTo(fImage, CV_32F);
     
-    float sum;
-    for(int y = 1; y < src.rows - 1; y++){
-        for(int x = 1; x < src.cols - 1; x++){
-            sum = 0.0;
-            for(int k = -1; k <= 1;k++){
-                for(int j = -1; j <=1; j++){
-                    sum = sum + Kernel[j+1][k+1] * src.at<uchar>(y - j, x - k);
-                }
-            }
-            dst.at<uchar>(y,x) = sum;
-        }
-    }
+    // FFT Image
+    std::cout << "Direct transform...\n";
+    cv::Mat fourierTransform;
+    cv::dft(fImage, fourierTransform, cv::DFT_SCALE|cv::DFT_COMPLEX_OUTPUT);
     
-    //ウィンドウの作成
-    cvNamedWindow("lpf" , CV_WINDOW_AUTOSIZE);
+    //filter
+    cv::Mat filter = inputImage.clone();
+    for(int y = 0; y < inputImage.rows; y++)
+        for(int x = 0; x < inputImage.cols; x++)
+            filter.at<uchar>(y,x) = 0.0;
     
-    IplImage iplImage = dst; // convert type
-
-    //画像をウィンドウに表示
-    cvShowImage("lpf", &iplImage);
+    for(int y = 0; y < 3; y++)
+        for(int x = 0; x < 3; x++)
+            filter.at<uchar>(y,x) = Kernel[y][x];
+            
     
-    //キーの入力待ち
+    // IFFT
+    std::cout << "Inverse transform...\n";
+    cv::Mat inverseTransform;
+    cv::dft(fourierTransform, inverseTransform, cv::DFT_INVERSE|cv::DFT_REAL_OUTPUT);
+    
+    // Back to 8-bits
+    cv::Mat finalImage;
+    inverseTransform.convertTo(finalImage, CV_8U);
+    
+    imshow("low-pass filter", finalImage);
+    
+    //Wait for key
     cvWaitKey();
     
-    //すべてのウィンドウを削除
+    //Destroy Window
     cvDestroyAllWindows();
-    
-    //画像データの開放
-    cvReleaseImage(&loadImg);
     
     return 0;
 }
