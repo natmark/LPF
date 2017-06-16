db001b2b2b326a7124a623539e960be3af769398
natmark <natmark0918@gmail.com>
2017-06-16 10:08:42 +0900

    :art: Use namespace

--- LPF/main.cpp
+++ LPF/main.cpp
@@ -14,24 +14,25 @@
 #endif
 
 using namespace std;
+using namespace cv;
 
 double pixelDistance(double u, double v)
 {
-    return cv::sqrt(u*u + v*v);
+    return sqrt(u*u + v*v);
 }
 
 double gaussianCoeff(double u, double v, double d0)
 {
     double d = pixelDistance(u, v);
-    return 1.0 - cv::exp((-d*d) / (2*d0*d0));
-    // return cv::exp((-d*d) / (2*d0*d0));
+    return 1.0 - exp((-d*d) / (2*d0*d0));
+    // return exp((-d*d) / (2*d0*d0));
 }
 
-cv::Mat createGaussianHighPassFilter(cv::Size size, double cutoffInPixels)
+Mat createGaussianHighPassFilter(Size size, double cutoffInPixels)
 {
-    cv::Mat ghpf(size, CV_64FC2);
+    Mat ghpf(size, CV_64FC2);
     
-    cv::Point center(size.width / 2, size.height / 2);
+    Point center(size.width / 2, size.height / 2);
     
     for(int u = 0; u < ghpf.rows; u++)
     {
@@ -43,13 +44,13 @@ cv::Mat createGaussianHighPassFilter(cv::Size size, double cutoffInPixels)
     
     return ghpf;
 }
-void shiftDFT(cv::Mat& fImage )
+void shiftDFT(Mat& fImage )
 {
-    cv::Mat tmp, q0, q1, q2, q3;
+    Mat tmp, q0, q1, q2, q3;
     
     // first crop the image, if it has an odd number of rows or columns
     
-    fImage = fImage(cv::Rect(0, 0, fImage.cols & -2, fImage.rows & -2));
+    fImage = fImage(Rect(0, 0, fImage.cols & -2, fImage.rows & -2));
     
     int cx = fImage.cols/2;
     int cy = fImage.rows/2;
@@ -57,10 +58,10 @@ void shiftDFT(cv::Mat& fImage )
     // rearrange the quadrants of Fourier image
     // so that the origin is at the image center
     
-    q0 = fImage(cv::Rect(0, 0, cx, cy));
-    q1 = fImage(cv::Rect(cx, 0, cx, cy));
-    q2 = fImage(cv::Rect(0, cy, cx, cy));
-    q3 = fImage(cv::Rect(cx, cy, cx, cy));
+    q0 = fImage(Rect(0, 0, cx, cy));
+    q1 = fImage(Rect(cx, 0, cx, cy));
+    q2 = fImage(Rect(0, cy, cx, cy));
+    q3 = fImage(Rect(cx, cy, cx, cy));
     
     q0.copyTo(tmp);
     q3.copyTo(q0);
@@ -72,35 +73,32 @@ void shiftDFT(cv::Mat& fImage )
 }
 int main(int argc, const char * argv[])
 {
-    // https://stackoverflow.com/questions/10269456/inverse-fourier-transformation-in-opencv
-    // http://org-technology.com/posts/smoother.html
-    
     // Load an image
-    cv::Mat inputImage = cv::imread("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", 0);
+    Mat inputImage = imread("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", 0);
     imshow("inputImage",inputImage);
     // Convert to float
-    cv::Mat fImage;
+    Mat fImage;
     inputImage.convertTo(fImage, CV_32F);
     
     // DFT Image
-    std::cout << "Direct transform...\n";
-    cv::Mat fourierTransform;
-    cv::dft(fImage, fourierTransform, cv::DFT_SCALE|cv::DFT_COMPLEX_OUTPUT);
+    cout << "Direct transform..." << endl;
+    Mat fourierTransform;
+    dft(fImage, fourierTransform, DFT_SCALE|DFT_COMPLEX_OUTPUT);
     
     //ここが怪しい。
-    cv::Mat highpassfilter = createGaussianHighPassFilter(cv::Size(fourierTransform.rows, fourierTransform.cols), 16.0);
+    Mat highpassfilter = createGaussianHighPassFilter(Size(fourierTransform.rows, fourierTransform.cols), 4.0);
     highpassfilter.convertTo(highpassfilter, 13);
     
-    shiftDFT(fourierTransform);
-    cv::multiply(fourierTransform, highpassfilter, fourierTransform);
+    mulSpectrums(fourierTransform, highpassfilter, fourierTransform, DFT_ROWS);
+    //multiply(fourierTransform, kernel, fourierTransform);
     
     // IDFT
-    std::cout << "Inverse transform...\n";
-    cv::Mat inverseTransform;
-    cv::dft(fourierTransform, inverseTransform, cv::DFT_INVERSE|cv::DFT_REAL_OUTPUT);
+    cout << "Inverse transform..." << endl;
+    Mat inverseTransform;
+    dft(fourierTransform, inverseTransform, DFT_INVERSE|DFT_REAL_OUTPUT);
     
     // Back to 8-bits
-    cv::Mat finalImage;
+    Mat finalImage;
     inverseTransform.convertTo(finalImage, CV_8U);
     
     imshow("low-pass filter", finalImage);
