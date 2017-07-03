621c48da22c489d566ecd3b34f9cd7851bd21b07
natmark <natmark0918@gmail.com>
2017-07-03 15:34:58 +0900

    :art: Fix program structure

--- LPF/main.cpp
+++ LPF/main.cpp
@@ -5,36 +5,44 @@
 //  Created by AtsuyaSato on 2017/06/03.
 //  Copyright © 2017年 Atsuya Sato. All rights reserved.
 //
-#include <opencv2/core/core.hpp>
-#include "opencv2/highgui/highgui.hpp"
-#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
+#include <unistd.h>
+
+#ifdef __cplusplus
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#endif
+
 using namespace std;
 using namespace cv;
 
 Mat shiftquads(Mat &magI,int cx,int cy){
-    Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-    Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-    Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-    Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-    
-    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+    Mat q0(magI, Rect(0, 0, cx, cy));
+    Mat q1(magI, Rect(cx, 0, cx, cy));
+    Mat q2(magI, Rect(0, cy, cx, cy));
+    Mat q3(magI, Rect(cx, cy, cx, cy));
+
+    Mat tmp;
     q0.copyTo(tmp);
     q3.copyTo(q0);
     tmp.copyTo(q3);
-    
-    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+
+    q1.copyTo(tmp);
     q2.copyTo(q1);
     tmp.copyTo(q2);
     return magI;
 }
 
-cv::Mat createGaussianHighPassFilter(Mat complex){
-    //    Mat ghpf(size, CV_32F);
+Mat createGaussianLowPassFilter(Mat complex){
     Mat ghpf( complex.size(), complex.type(), CV_32F);
+
     int midx = ghpf.rows / 2;
     int midy = ghpf.cols / 2;
-    int d0 = 50;
+
+    int d0 = 20; //threshold
+    
     for(int u = 0; u < ghpf.rows; u++)
     {
         for(int v = 0; v < ghpf.cols; v++)
@@ -43,82 +51,49 @@ cv::Mat createGaussianHighPassFilter(Mat complex){
                 ghpf.at<double>(u,v)=0.0;
         }
     }
+
     shiftquads(ghpf,midx,midy);
     return ghpf;
 }
 
-
-void show_spectrum(Mat complexI,Mat planes[],string message){
-    // compute the magnitude and switch to logarithmic scale
-    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-    
-    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-    Mat magI = planes[0];
-    
-    magI += Scalar::all(1);                    // switch to logarithmic scale
-    log(magI, magI);
-    
-    // crop the spectrum, if it has an odd number of rows or columns
-    magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-    
-    // rearrange the quadrants of Fourier image  so that the origin is at the image center
-    int cx = magI.cols/2;
-    int cy = magI.rows/2;
-    
-    Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-    Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-    Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-    Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-    
-    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-    q0.copyTo(tmp);
-    q3.copyTo(q0);
-    tmp.copyTo(q3);
-    
-    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-    q2.copyTo(q1);
-    tmp.copyTo(q2);
-    
-    
-    normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-    // viewable image form (float between values 0 and 1).
-    
-    imshow(message +" Spectrum" , magI);
-    
-}
 int main(int argc, char** argv)
 {
-    Mat I = imread("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", 0);
-    if( I.empty())
-    {
-        return -1;
-    }
+    // Load an image
+    Mat inputImage = imread("/Users/AtsuyaSato/Desktop/LPF/lena.jpg", 0);
+    imshow("inputImage",inputImage);
     
-    Mat padded;                            //expand input image to optimal size
-    int m = getOptimalDFTSize( I.rows );
-    int n = getOptimalDFTSize( I.cols ); // on the border add zero values
-    copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
+    //expand input image to optimal size
+    Mat padded;
+    int m = getOptimalDFTSize( inputImage.rows );
+    int n = getOptimalDFTSize( inputImage.cols );
+    
+    copyMakeBorder(inputImage, padded, 0, m - inputImage.rows, 0, n - inputImage.cols, BORDER_CONSTANT, Scalar::all(0));
     
     Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
     Mat complexI;
-    merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+    merge(planes, 2, complexI);
     
-    dft(complexI, complexI);            // this way the result may fit in the source matrix
+    // DFT
+    Mat fourierTransform;
+    dft(complexI, fourierTransform);
     
-    imshow("Input Image"       , I   );    // Show the result
-    show_spectrum(complexI,planes,"input");
+    // lowPassFilter
+    Mat lowPassFilter = createGaussianLowPassFilter(fourierTransform);
+    cv::mulSpectrums(fourierTransform,lowPassFilter,fourierTransform,DFT_COMPLEX_OUTPUT);
     
-    Mat ghpf = createGaussianHighPassFilter(complexI);
-    cv::mulSpectrums(complexI,ghpf,complexI,DFT_COMPLEX_OUTPUT);
+    // IDFT
+    Mat inverseTransform;
+    dft(fourierTransform, inverseTransform, DFT_INVERSE|DFT_REAL_OUTPUT);
     
-    show_spectrum(complexI,planes,"output");
-    
-    cv::Mat inverseTransform;
-    cv::dft(complexI, inverseTransform, cv::DFT_INVERSE|cv::DFT_REAL_OUTPUT);
     normalize(inverseTransform, inverseTransform, 0, 1, CV_MINMAX);
-    imshow("Reconstructed", inverseTransform);
+
+    imshow("Filtered Image", inverseTransform);
+    
+    // Wait for key
     waitKey();
+    
+    // Destroy Window
+    cvDestroyAllWindows();
     
     return 0;
 }
